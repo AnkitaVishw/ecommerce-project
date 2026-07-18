83a181c863984a04063e690bd52ad52e08effda1
AnkitaVishw <114240903+AnkitaVishw@users.noreply.github.com>
2026-07-18 14:57:37 +0530

    Revert "Added seller login functionalityFeature/authentication"

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,3 @@
-
 from django.contrib import admin
-from .models import Customer, Seller
 
-admin.site.register(Customer)
-admin.site.register(Seller)
+# Register your models here.
--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class Customer(models.Model):
-    pass
-
-
-class Seller(models.Model):
-    pass
