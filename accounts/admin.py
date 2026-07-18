3a6583106c8b1f4830337266d8e04d12cb85d6b2
AnkitaVishw <114240903+AnkitaVishw@users.noreply.github.com>
2026-07-18 22:02:31 +0530

    Development (#18)
    * Add Category and Product models (#16)
    
    * Add custom user model and restore migrations
    
    * Authentication related

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
 from .models import User
 
 
-admin.site.register(User, UserAdmin)
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Information", {
+            "fields": ("phone", "role", "shop_name"),
+        }),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {
+            "fields": ("phone", "role", "shop_name"),
+        }),
+    )
+
+    list_display = (
+        "username",
+        "email",
+        "role",
+        "phone",
+        "is_staff",
+    )
--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,17 @@
 from django import forms
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import User
 
 
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
+class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = [
+            "username",
+            "email",
+            "phone",
+            "role",
+            "shop_name",
+            "password1",
+            "password2",
+        ]
