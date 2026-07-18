68447142444cd17dd17fc7b28ab5bbb5d19db99e
Ankita Vishwakarma <ankushrma1999@gmail.com>
2026-07-18 21:57:03 +0530

    Authentication related

--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,46 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from .forms import RegisterForm
+from .forms import UserRegistrationForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def profile(request):
+    return render(request, "accounts/profile.html")
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect("profile")
+        else:
+            messages.error(request, "Invalid username or password")
+
+    return render(request, "accounts/login.html")
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
 
 def register(request):
     if request.method == "POST":
-        form = RegisterForm(request.POST)
+        form = UserRegistrationForm(request.POST)
 
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect("/")
+            form.save()
+            return redirect("login")
 
     else:
-        form = RegisterForm()
+        form = UserRegistrationForm()
 
     return render(request, "accounts/register.html", {"form": form})
