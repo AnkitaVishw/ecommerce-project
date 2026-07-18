da232653f271df9fb719168736b9c596b4fee8b0
AnkitaVishw <114240903+AnkitaVishw@users.noreply.github.com>
2026-07-18 14:55:27 +0530

    Revert "update customer and seller model"

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,6 @@
 
-
-
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import Customer, Seller
 
-admin.site.register(CustomUser, UserAdmin)
+admin.site.register(Customer)
+admin.site.register(Seller)
