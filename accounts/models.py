234793f4b6cb0ec86b22d2d60d2853e4df011efb
Ankita Vishwakarma <ankushrma1999@gmail.com>
2026-07-18 19:26:26 +0530

    Add custom user model and restore migrations

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    CUSTOMER = "customer"
+    SELLER = "seller"
+
+    ROLE_CHOICES = [
+        (CUSTOMER, "Customer"),
+        (SELLER, "Seller"),
+    ]
+
+    phone = models.CharField(max_length=15, blank=True)
+
+    role = models.CharField(
+        max_length=10,
+        choices=ROLE_CHOICES,
+        default=CUSTOMER
+    )
+
+    shop_name = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.username
--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Address, Cart, CartItem, Order, OrderItem
 
-# Register your models here.
+admin.site.register(Address)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
