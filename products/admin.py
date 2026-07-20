234411316fb47c2ea4a49467183f1ccb65fa1557
Ankita Vishwakarma <ankushrma1999@gmail.com>
2026-07-20 15:48:41 +0530

    Added Product list

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from .models import Product
+from .models import Category, Product
 
-admin.site.register(Product)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "category", "price", "stock")
+    list_filter = ("category",)
+    search_fields = ("name",)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Product
 
-# Create your views here.
+@login_required
+def product_list(request):
+    products = Product.objects.select_related("category").all()
+
+    return render(
+        request,
+        "products/product_list.html",
+        {
+            "products": products
+        }
+    )
