61ab7d21d45ac02daf69eb446045540351df83e3
Ankita Vishwakarma <ankushrma1999@gmail.com>
2026-07-21 21:18:23 +0530

    Add ecommerce homepage UI and product detail page

--- products/views.py
+++ products/views.py
@@ -1,15 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Product
+
 
 @login_required
 def product_list(request):
     products = Product.objects.select_related("category").all()
 
+    context = {
+        "products": products
+    }
+
     return render(
         request,
         "products/product_list.html",
-        {
-            "products": products
-        }
+        context
+    )
+
+
+@login_required
+def product_detail(request, pk):
+
+    product = get_object_or_404(Product, pk=pk)
+
+    related_products = Product.objects.filter(
+        category=product.category
+    ).exclude(pk=pk)[:4]
+
+    context = {
+        "product": product,
+        "related_products": related_products,
+    }
+
+    return render(
+        request,
+        "products/product_detail.html",
+        context,
     )
